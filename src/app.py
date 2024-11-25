fc003b75d26b2a2374755270eb65efce275cc005
Mark O'Neill <aoneillmark@gmail.com>
2024-11-25 14:50:51 +0000

    fixed app.py not working

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
-# import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    # return "Hello World!"
     return render_template("index.html")
 
 @app.route("/library")
 def library():
     return render_template("library.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
