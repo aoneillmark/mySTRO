70817097c66cc4c711e5e3da8709aa082d55e946
Miriam Tiefel <mst124@ic.ac.uk>
2024-11-25 17:16:29 +0000

    added musicpiece.py 2

--- models/musicpiece.py
+++ models/musicpiece.py
@@ -1,7 +1,8 @@
 from library.database import database as db
 
+
 class MusicPiece(db.Model):
-    __tablename__ = 'music_piece'
+    __tablename__ = "music_piece"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     composer = db.Column(db.String(80), nullable=False)
