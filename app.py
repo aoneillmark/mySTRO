d3ae38eb111d431a544e9e64e71c8fc460162c64
Mark O'Neill <aoneillmark@gmail.com>
2024-11-25 14:51:53 +0000

    Whoops wrong app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World!"
-    #return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/library")
+def library():
+    return render_template("library.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
