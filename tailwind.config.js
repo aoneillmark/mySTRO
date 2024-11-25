ecd21a1b8c777ce2eaebc3bbd03f447c63a5555b
Mark O'Neill <aoneillmark@gmail.com>
2024-11-25 15:36:19 +0000

    Added a bunch of tailwind stuff

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/templates/**/*.html', './src/static/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        pumpkin: '#ff6700ff',
+        'penn-red': '#92140cff',
+        'dark-purple': '#2f0a28ff',
+        linen: '#f3e9dcff',
+        'battleship-gray': '#76877dff',
+      },
+    },
+  },
+  plugins: [],
+}
