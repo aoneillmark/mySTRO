a25865cc8b91c4e8c24436b4f419e4202c77b0b0
Asal Shams <as3824@ic.ac.uk>
2024-11-25 16:22:11 +0000

    Succesfully uploaded first dropdown of composers

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="src/templates")
 
 COMPOSERS = [
     "John Adams", "Thomas Adès", "Isaac Albéniz", "Tomaso Albinoni", "George Antheil",
