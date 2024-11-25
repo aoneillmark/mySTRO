f060ed04cba876238cd1588f241d7aae10c759d7
Mark O'Neill <aoneillmark@gmail.com>
2024-11-25 16:40:28 +0000

    added specification for template_folder

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
+app = Flask(__name__, template_folder='src/templates')
 
 @app.route("/")
 def hello_world():
