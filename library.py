a3774df4a24cc9f4b60e22bd223566583390c95d
Miriam Tiefel <mst124@ic.ac.uk>
2024-11-25 16:59:18 +0000

    Python formatting library.py 2

--- library.py
+++ library.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from sqlalchemy import select
 
 from app.database import database
