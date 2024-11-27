2fbb5e4488a8eb51908f54a6e9c4ceed94959b2b
Miriam Tiefel <mst124@ic.ac.uk>
2024-11-27 14:32:07 +0000

    Trying to solve db error

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Set up extensions
-database.init_app(app)
+database.db.init_app(app)
 
 # Register blueprints
 import Blueprint as blueprints
