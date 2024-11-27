a00e270862fab72b53309562a733a1ade556786c
Miriam Tiefel <mst124@ic.ac.uk>
2024-11-27 12:14:57 +0000

    reformatted cli.py

--- cli.py
+++ cli.py
@@ -10,30 +10,33 @@ from models.musicpiece import MusicPiece
 def create_all():
     database.create_all()
 
+
 @click.command("drop_all", help="Drop all tables in the specified database")
 @with_appcontext
 def drop_all():
     database.drop_all()
 
 
-@click.command("populate", help="Populate the database with initial data") # Maybe this is placeholder?
+@click.command(
+    "populate", help="Populate the database with initial data"
+)  # Maybe this is placeholder?
 @with_appcontext
 def populate():
     initial_music_pieces = [
         MusicPiece(
-            id = 1,
+            id=1,
             title="PLACEHOLDER Sonata No. 14",
             composer="Ludwig van Beethoven",
             genre="Classical",
             description="A beautiful piano sonata composed by Beethoven. :3",
         ),
         MusicPiece(
-            id = 2,
+            id=2,
             title="PLACEHOLDER Symphony No. 5",
             composer="Ludwig van Beethoven",
             genre="Classical",
             description="A symphony composed by Beethoven.",
-        )
+        ),
     ]
     for piece in initial_music_pieces:
         database.session.add(piece)
