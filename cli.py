dae29e2b2b6fffb3b8015772c6084808ef6651aa
Mark O'Neill <aoneillmark@gmail.com>
2024-11-28 12:19:44 +0000

    added print statements to cli

--- cli.py
+++ cli.py
@@ -8,30 +8,29 @@ from models.musicpiece import MusicPiece
 @click.command("create_all", help="Create all tables in the app's databases")
 @with_appcontext
 def create_all():
+    print("Creating all tables in the database...")
     database.create_all()
 
 
 @click.command("drop_all", help="Drop all tables in the specified database")
 @with_appcontext
 def drop_all():
+    print("Dropping all tables in the database...")
     database.drop_all()
 
 
-@click.command(
-    "populate", help="Populate the database with initial data"
-)  # Maybe this is placeholder?
+@click.command("populate", help="Populate the database with initial data")
 @with_appcontext
 def populate():
+    print("Populating the database with initial data...")
     initial_music_pieces = [
         MusicPiece(
-            id=1,
             title="PLACEHOLDER Sonata No. 14",
             composer="Ludwig van Beethoven",
             genre="Classical",
             description="A beautiful piano sonata composed by Beethoven. :3",
         ),
         MusicPiece(
-            id=2,
             title="PLACEHOLDER Symphony No. 5",
             composer="Ludwig van Beethoven",
             genre="Classical",
@@ -41,3 +40,4 @@ def populate():
     for piece in initial_music_pieces:
         database.session.add(piece)
     database.session.commit()
+    click.echo("Database populated with initial data.")
